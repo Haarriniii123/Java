23f6f2593f61460bace73e11a2daa4c4a95e41c4
Haarriniii123 <harinithangaraj65@gmail.com>
2022-03-10 18:51:37 +0530

    Testng proj

---
+++ src/test/java/com/Sample/SamplePgm.java
@@ -0,0 +1,15 @@
+package com.Sample;
+
+import org.testng.annotations.Test;
+
+public class SamplePgm {
+	
+	@Test
+	private void emplyeeDetails() {
+		
+		System.out.println("Employee name is ");
+		
+
+	}
+
+}
